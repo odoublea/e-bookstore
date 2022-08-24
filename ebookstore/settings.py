8fc287c6ace15a9ed246efbc72eb29f0fce48d97
Oladega Abdulqudus <oladegakenny@gmail.com>
2022-08-24 15:01:34 +0100

    requirements updated

--- ebookstore/settings.py
+++ ebookstore/settings.py
@@ -88,11 +88,8 @@ WSGI_APPLICATION = 'ebookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookstore',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
